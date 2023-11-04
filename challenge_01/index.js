b47d59896b2e96cd6c4818903c83934faa1fdb11
Roy Willynson Del Orbe Medina <roywillinsondelorbemedina@gmail.com>
2023-11-04 15:31:03 -0400

    First Challenge

---
+++ challenge_01/index.js
@@ -0,0 +1,34 @@
+const fs = require('fs/promises')
+const path = require('path')
+
+function countWords(text) {
+  const values = text.toLowerCase().split(' ')
+
+  const valuesMap = {}
+
+  for (const value of values) {
+    if (valuesMap[value] === undefined) {
+      valuesMap[value] = 0
+    }
+
+    valuesMap[value]++
+  }
+
+  return valuesMap
+}
+
+function formatCountWords(object) {
+  return Object.entries(object).reduce((acc, value) => {
+    const [word, count] = value
+
+    return `${acc}${word}${count}`
+  }, '')
+}
+
+;(async function () {
+  // TEST MY CODE
+  const message = await fs.readFile(path.resolve(__dirname, 'message_01.txt'), {
+    encoding: 'utf-8',
+  })
+  console.log(formatCountWords(countWords(message)))
+})()
